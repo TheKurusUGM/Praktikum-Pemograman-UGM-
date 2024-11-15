5dc7ed6d0e07b2740d03b7d1a743960f4bae86e3
Avicena Yosanta Muchtar <yosasyafana@gmail.com>
2024-11-15 15:01:27 +0700

    Create changename.cpp

---
+++ Week_8/Problem_3/changename.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+struct Student {
+    long long NISN;
+    std::string Nama;
+    int Value;
+};
+
+void displayData(const std::vector<Student>& students) {
+    for (const auto& student : students) {
+        std::cout << student.NISN << " " << student.Nama << " " << student.Value << std::endl;
+    }
+}
+
+
+void updateNameByValue(std::vector<Student>& students, int targetValue, const std::string& newName) {
+    for (auto& student : students) {
+        if (student.Value == targetValue) {
+            student.Nama = newName; 
+        }
+    }
+}
+
+int main() {
+    std::vector<Student> students = {
+        {9960312699, "Handi Ramadhan", 90},
+        {9963959682, "Rio Alfandra", 55},
+        {9950310962, "Ronaldo Valentino Uneputty", 80},
+        {9970272750, "Achmad Yaumil Fadjri R.", 60},
+        {9970293945, "Alivia Rahma Pramesti", 70},
+        {9952382180, "Ari Lutfianto", 65},
+        {9965653989, "Arief Budiman", 60}
+    };
+
+    std::cout << "Original Data:\n";
+    displayData(students);
+
+ 
+    updateNameByValue(students, 60, "Joko");
+
+    std::cout << "\nData after updating names with Value 60 to 'Joko':\n";
+    displayData(students);
+
+    return 0;
+}

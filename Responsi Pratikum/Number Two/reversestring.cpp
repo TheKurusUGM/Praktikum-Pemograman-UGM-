950ea8a11dec0128a3238087f9202fd30f60dec7
Avicena Yosanta Muchtar <yosasyafana@gmail.com>
2024-11-25 15:20:39 +0700

    Update reversestring.cpp

--- Responsi Pratikum/Number Two/reversestring.cpp
+++ Responsi Pratikum/Number Two/reversestring.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <string>
 
+int main() {
+    std::string input = "1,2,3,4,5"; 
+    std::cout << "Original string: " << input << std::endl;
+    std::cout << "Reversed string: ";
+
+    for (std::size_t i = input.length(); i > 0; --i) {
+        std::cout << input[i - 1];
+    }
+    std::cout << std::endl;
+    return 0;
+}
